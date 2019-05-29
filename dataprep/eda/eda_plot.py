c18344bcadd5d7e6885a446b13f4d7025f420200
SHUBHAM LADDHA <laddha.shubham97@gmail.com>
2019-05-29 15:31:05 -0700

    refactor(eda.plot): plot function combined into one.

---
+++ dataprep/eda/eda_plot.py
@@ -0,0 +1,318 @@
+"""
+    This module implements the plot(df) function.
+"""
+import itertools
+import logging
+from typing import Dict, List, Union, Optional
+
+# noinspection PyUnresolvedReferences
+import dask
+import dask.array as da
+import dask.dataframe as dd
+import numpy as np
+import pandas as pd
+
+
+def calc_box(dataframe: pd.DataFrame, col_x: str,
+             col_y: str) -> Dict[str, dict]:
+    """ Returns intermediate stats of the box plot
+            of columns col_x and col_y.
+
+    PARAMETERS
+    __________
+    dataframe: the input dataframe
+    col_x : a valid column name of the dataframe
+    col_y : a valid column name of the dataframe
+
+
+    RETURNS
+    __________
+    a (column_name: data) dict storing the intermediate results
+    """
+    cat_col, num_col = (col_x, col_y) if (get_type(dataframe[col_x]) ==
+                                          'TYPE_CAT') else (col_y, col_x)
+
+    dask_df = dd.from_pandas(dataframe, npartitions=1)
+    res = dict()
+    stats = dict()
+
+    for group in dask_df[cat_col].unique().compute():
+        stats = {}
+        grp_series = dask_df.groupby(cat_col).get_group(group)[num_col]
+        quantiles = grp_series.quantile([.25, .50, .75]).compute()
+        stats['25%'], stats['50%'], stats['75%'] = dask.delayed(
+            quantiles[.25]), dask.delayed(quantiles[.50]), dask.delayed(quantiles[.75])
+        stats['iqr'] = stats['75%'] - stats['25%']
+        outliers = list()
+        grp_series = grp_series.compute()
+        if len(grp_series) == 1:
+            stats['min'] = grp_series.reset_index().iloc[0, 1]
+            stats['max'] = stats['min']
+        else:
+            min_value = np.inf
+            max_value = -np.inf
+            for value in filter(
+                    lambda row: stats['25%'].compute() - (1.5 * stats['iqr'].compute()) < row < \
+                        stats['75%'].compute() + (1.5 * stats['iqr'].compute()), grp_series):
+                min_value = value if value < min_value else min_value
+                max_value = value if value > max_value else max_value
+
+            for value in itertools.filterfalse(
+                    lambda row: stats['25%'].compute() - (1.5 * stats['iqr'].compute()) < row < \
+                        stats['75%'].compute() + (1.5 * stats['iqr'].compute()), grp_series):
+                outliers.append(value)
+            stats['min'] = min_value
+            stats['max'] = max_value
+
+        stats['outliers'] = outliers
+
+        res[group] = stats
+
+    res, = dask.compute(res)
+    return res
+
+
+def calc_statcked(dataframe: pd.DataFrame, col_x: str, col_y: str) \
+        -> Dict[tuple, int]:
+    """ Returns intermediate stats of the stacked column plot
+            of columns col_x and col_y.
+
+    PARAMETERS
+    __________
+    dataframe: the input dataframe
+    col_x : a valid column name of the dataframe
+    col_y : a valid column name of the dataframe
+
+
+    RETURNS
+    __________
+    a (column_name: data) dict storing the intermediate results
+    """
+
+    dask_df = dask.dataframe.from_pandas(dataframe, npartitions=1)
+    grp_object = dask_df.groupby([col_x, col_y])
+
+    grp_series = grp_object.count().compute().iloc[:, 0]
+    # print (grp_series)
+    return dict(grp_series)
+
+
+def calc_scatter(dataframe: pd.DataFrame, col_x: str,
+                 col_y: str) -> Dict[Union[int, float], Union[int, float]]:
+    """
+        TO-DO: WARNING: For very large amount of points, implement Heat Map.
+        Returns intermediate stats of the scattered plot
+        of columns col_x and col_y.
+
+        PARAMETERS
+        __________
+        dataframe: the input dataframe
+        col_x : a valid column name of the dataframe
+        col_y : a valid column name of the dataframe
+
+
+        RETURNS
+        __________
+        a (column_name: data) dict storing the intermediate results
+    """
+    dask_df = dask.dataframe.from_pandas(dataframe, npartitions=1)
+    series_x = dask_df[col_x].compute()
+    series_y = dask_df[col_y].compute()
+
+    res = set()
+    for each in zip(series_x, series_y):
+        res.add(each)
+
+    return dict(res)
+
+
+def calc_count(dataframe: pd.DataFrame, col: str) -> Dict[str, int]:
+    """ Returns a dict {category: category_count} for the
+        categorical column given as the second argument
+
+    Parameters
+    __________
+    dataframe : the input pandas dataframe
+    col : the str column of dataframe for which count needs to be calculated
+
+    Returns
+    __________
+    dict : A dict of (category : count) for the input col
+    """
+    dask_df = dd.from_pandas(dataframe, npartitions=1)
+    grp_object = dask_df.groupby(col)[col].count()
+    return dict(grp_object.compute())
+
+
+def calc_hist(dataframe: pd.DataFrame, col: str,
+              nbins: int = 10) -> List[float]:
+    """Returns the histogram array for the continuous
+        distribution of values in the column given as the second argument
+
+    Parameters
+    __________
+    dataframe : the input pandas dataframe
+    col : the str column of dataframe for which hist array needs to be
+    calculated
+
+    Returns
+    __________
+    np.array : An array of values representing histogram for the input col
+    """
+    minv = dataframe[col].min()
+    maxv = dataframe[col].max()
+    dframe = dd.from_array(dataframe[col]).dropna()
+    hist_array, _ = da.histogram(dframe.values, range=[minv, maxv], bins=nbins)
+    return hist_array.compute()
+
+
+logging.basicConfig(level=logging.INFO, format="%(message)")
+LOGGER = logging.getLogger(__name__)
+
+
+def get_type(data: pd.Series) -> str:
+    """ Returns the type of the input data.
+        Identified types are:
+        'TYPE_CAT' - if data is categorical.
+        'TYPE_NUM' - if data is numeric.
+        'TYPE_UNSUP' - type not supported.
+         TO-DO
+
+    Parameter
+    __________
+    The data for which the type needs to be identified.
+
+    Returns
+    __________
+    str representing the type of the data.
+    """
+
+    col_type = 'TYPE_UNSUP'
+    try:
+        if pd.api.types.is_bool_dtype(data):
+            col_type = 'TYPE_CAT'
+        elif pd.api.types.is_numeric_dtype(data) and data.count() == 2:
+            col_type = 'TYPE_CAT'
+        elif pd.api.types.is_numeric_dtype(data):
+            col_type = 'TYPE_NUM'
+        else:
+            col_type = 'TYPE_CAT'
+    except NotImplementedError as error:    #TO-DO
+        LOGGER.info("Type cannot be determined due to : %s", error)
+
+    return col_type
+
+
+# Type aliasing
+String_List = List[str]
+
+
+def plot_df(data_frame: pd.DataFrame, force_cat: Optional[String_List] = None,
+            force_num: Optional[String_List] = None) -> dict:
+    """
+        Supporting funtion to the main plot function
+    :param data_frame: pandas dataframe
+    :param force_cat: list of categorical columns defined explicitly
+    :param force_num: list of numerical columns defined explicitly
+    :return:
+    """
+    col_list = []
+    dask_result = list()
+
+    for col in data_frame.columns:
+        if data_frame[col].count() == 0:
+            col_list.append(col)
+            dask_result.append([])
+            continue
+
+        elif get_type(data_frame[col]) == 'TYPE_CAT' or (
+                force_cat is not None and col in force_cat):
+            cnt_series = dask.delayed(calc_count)(data_frame, col)
+            dask_result.append(cnt_series)
+            col_list.append(col)
+
+        elif get_type(data_frame[col]) == 'TYPE_NUM' or (
+                force_num is not None and col in force_num):
+            hist = dask.delayed(calc_hist)(data_frame, col)
+            dask_result.append(hist)
+            col_list.append(col)
+
+    column_dict = dict()
+    computed_res, = dask.compute(dask_result)
+
+    for each in zip(col_list, computed_res):
+        column_dict[each[0]] = each[1]
+
+    return column_dict
+
+def plot(data_frame: pd.DataFrame, col_x: Optional[str] = None,
+         col_y: Optional[str] = None, force_cat: Optional[String_List] = None,
+         force_num: Optional[String_List] = None
+        ) -> dict:
+    """
+    Returns an intermediate representation for the plots of
+        different columns in the data_frame.
+
+    Parameters
+    data_frame: the pandas data_frame for which plots are calculated for each
+    column.
+    col_x : A column in the data_frame.
+    col_y : A column in the data_frame.
+    force_cat: the list of columns which have to considered of type 'TYPE_CAT'
+    force_num: the list of columns which have to considered of type 'TYPE_NUM'
+    kwargs : TO-DO
+
+    Returns
+    __________
+    dict : A (column: [array/dict]) dict to encapsulate the
+    intermediate results.
+    """
+    result = dict()
+
+    if col_x is None and col_y is None:
+        result = plot_df(data_frame, force_cat, force_num)
+
+    elif col_x is None and col_y is not None or col_x is not None and col_y is None:
+
+        target_col = col_x if col_y is None else col_y
+        dask_result = list()
+
+        if data_frame[target_col].count() == 0:
+            dask_result.append([])
+
+        elif get_type(data_frame[target_col]) == 'TYPE_CAT' or (
+                force_cat is not None and target_col in force_cat):
+            dask_result.append(dask.delayed(calc_count)(data_frame, target_col))
+
+        elif get_type(data_frame[target_col]) == 'TYPE_NUM' or (
+                force_num is not None and target_col in force_num):
+            dask_result.append(dask.delayed(calc_hist)(data_frame, target_col))
+
+        column_dict = {target_col: dask.compute(dask_result)}
+        result = column_dict
+
+    elif col_x is not None and col_y is not None:
+        type_x = get_type(data_frame[col_x])
+        type_y = get_type(data_frame[col_y])
+
+        try:
+            if type_y == 'TYPE_CAT' and type_x == 'TYPE_NUM' or \
+                    type_y == 'TYPE_NUM' and type_x == 'TYPE_CAT':
+                result = calc_box(data_frame, col_x, col_y)
+
+            elif type_x == 'TYPE_CAT' and type_y == 'TYPE_CAT':
+                result = calc_statcked(data_frame, col_x, col_y)
+
+            elif type_x == 'TYPE_NUM' and type_y == 'TYPE_NUM':
+                result = calc_scatter(data_frame, col_x, col_y)
+            else:
+                pass
+                # WARNING: TO-DO
+        except NotImplementedError as error:   #TO-DO
+            LOGGER.info("Plot could not be obtained due to : %s", error)
+            result = dict()
+    else:
+        pass
+        # TO-DO to be added
+
+    return result
--- dataprep/tests/eda/test_eda.py
+++ dataprep/tests/eda/test_eda.py
@@ -1,62 +1,189 @@
+"""
+    module for testing plot(df, x, y) function.
+"""
 import datetime
 
 import numpy as np
 import pandas as pd
+from pandas import Timestamp
 
-from ...eda.EDA_plot import give_count, give_hist, plot
+from ...eda.eda_plot import plot  # dataprep.tests.eda.test_eda
 
 
-class TestClass(object):
+def test_normal():
+    """
 
-    def test_normal(self):
-        self.data_1 = {
+    :return:
+    """
+    data_1 = {
 
-            'id': [chr(97 + c) for c in range(1, 10)],
+        'id': [chr(97 + c) for c in range(1, 10)],
 
-            'x': [50, 50, -10, 0, 0, 5, 15, -3, None],
+        'x': [50, 50, -10, 0, 0, 5, 15, -3, None],
 
-            'y': [0.000001, 654.152, None, 15.984512, 3122, -3.1415926535, 111, 15.9, 13.5],
+        'y': [0.000001, 654.152, None, 15.984512, 3122, -3.1415926535, 111,
+              15.9, 13.5],
 
-            's1': np.ones(9),
+        's1': np.ones(9),
 
-            'somedate': [datetime.date(2011, 7, 4), datetime.datetime(2022, 1, 1, 13, 57), datetime.datetime(1990, 12, 9), np.nan, datetime.datetime(1990, 12, 9), datetime.datetime(1950, 12, 9), datetime.datetime(1898, 1, 2), datetime.datetime(1950, 12, 9), datetime.datetime(1950, 12, 9)],
+        'somedate': [datetime.date(2011, 7, 4),
+                     datetime.datetime(2022, 1, 1, 13, 57),
+                     datetime.datetime(1990, 12, 9), np.nan,
+                     datetime.datetime(1990, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1898, 1, 2),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9)],
 
-            'bool_tf': [True, True, False, True, False, True, True, False, True],
+        'bool_tf': [True, True, False, True, False, True, True, False,
+                    True],
 
-            'bool_tf_with_nan': [True, False, False, False, False, True, True, False, np.nan],
+        'bool_tf_with_nan': [True, False, False, False, False, True, True,
+                             False, np.nan],
 
-            'bool_01': [1, 1, 0, 1, 1, 0, 0, 0, 1],
+        'bool_01': [1, 1, 0, 1, 1, 0, 0, 0, 1],
 
-            'bool_01_with_nan': [1, 0, 1, 0, 0, 1, 1, 0, np.nan],
+        'bool_01_with_nan': [1, 0, 1, 0, 0, 1, 1, 0, np.nan],
 
-            'mixed': [1, 2, "a", 4, 5, 6, 7, 8, 9]
+        'mixed': [1, 2, "a", 4, 5, 6, 7, 8, 9]
 
-        }
+    }
 
-        self.df_1 = pd.DataFrame(self.data_1)
+    df_1 = pd.DataFrame(data_1)
 
-        self.df_1_expected = {'bool_01': {0: 4, 1: 5}, 'bool_01_with_nan': {0.0: 4, 1.0: 4}, 'bool_tf': {False: 3, True: 6}, 'bool_tf_with_nan': {
-            False: 5, True: 3}, 's1': {1.0: 9}, 'x': np.array([1, 3, 1, 0, 1, 0, 0, 0, 0, 2], dtype=np.int64), 'y': np.array([6, 0, 1, 0, 0, 0, 0, 0, 0, 1], dtype=np.int64)}
-        res = plot(self.df_1, force_cat=['bool_01', 'bool_01_with_nan', 's1'])
-        assert res['bool_01'] == self.df_1_expected['bool_01']
-        assert res['bool_01_with_nan'] == self.df_1_expected['bool_01_with_nan']
-        assert res['bool_tf'] == self.df_1_expected['bool_tf']
-        assert res['bool_tf_with_nan'] == self.df_1_expected['bool_tf_with_nan']
-        assert res['s1'] == self.df_1_expected['s1']
-        assert np.all(res['x'] == self.df_1_expected['x'])
-        assert np.all(res['y'] == self.df_1_expected['y'])
+    df_1_expected = {'bool_01': {0: 4, 1: 5},
+                     'bool_01_with_nan': {0.0: 4, 1.0: 4},
+                     'bool_tf': {False: 3, True: 6},
+                     'bool_tf_with_nan': {
+                         False: 5, True: 3}, 's1': {1.0: 9},
+                     'x': np.array([1, 3, 1, 0, 1, 0, 0, 0, 0, 2],
+                                   dtype=np.int64),
+                     'y': np.array([6, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+                                   dtype=np.int64)}
+    res = plot(df_1, force_cat=['bool_01', 'bool_01_with_nan', 's1'])
 
-    def test_corner(self):
+    assert res['bool_01'] == df_1_expected['bool_01']
+    assert res['bool_01_with_nan'] == df_1_expected['bool_01_with_nan']
+    assert res['bool_tf'] == df_1_expected['bool_tf']
+    assert res['bool_tf_with_nan'] == df_1_expected['bool_tf_with_nan']
+    assert res['s1'] == df_1_expected['s1']
+    assert np.all(res['x'] == df_1_expected['x'])
+    assert np.all(res['y'] == df_1_expected['y'])
 
-        self.df_2 = pd.DataFrame({'all_nan': [np.nan for _ in range(10)], 'all_one': np.ones(10), 'all_zeros': np.zeros(10), 'random': np.array([0.38538395,  0.13609054,  0.15973238,  0.96192966,  0.03708882,
-                                                                                                                                                 0.03633855,  0.25260128,  0.72139843,  0.74553949,  0.41102021])})
+    data = {
 
-        self.df_1_expected = {'all_one': {1.0: 10},
-                              'all_zeros': {0.0: 10},
-                              'random': np.array([2, 2, 1, 1, 1, 0, 0, 2, 0, 1], dtype=np.int64)}
+        'id': [chr(97 + c) for c in range(1, 21)],
 
-        res = plot(self.df_2, force_cat=['all_one', 'all_zeros'])
+        'x': ['d', 'c', 'b', 'a', 'b', 'd', 'c', 'a', 'a', 'a', 'c', 'b',
+              'c', 'a', 'd', 'b', 'b', 'b', 'b', 'b'],
 
-        #assert np.all(res['all_nan'] == self.df_1_expected['all_nan'])
-        assert res['all_one'] == self.df_1_expected['all_one']
-        assert res['all_zeros'] == self.df_1_expected['all_zeros']
+        'y': [794, 652, 158, 134, 448, 682, 135, 795, 353, 395, 403, 498,
+              622, 80, 654, 772, 867, 676, 670, 736],
+
+        's1': np.ones(20),
+
+        'somedate': [datetime.date(2011, 7, 4),
+                     datetime.datetime(1898, 1, 2),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1898, 1, 2),
+                     datetime.datetime(1990, 12, 9), np.nan,
+                     datetime.datetime(1990, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1898, 1, 2),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9),
+                     datetime.datetime(1950, 12, 9)],
+    }
+
+    df_data = pd.DataFrame(data)
+
+    df_expected = {'a': {'25%': 134.0,
+                         '50%': 353.0,
+                         '75%': 395.0,
+                         'iqr': 261.0,
+                         'max': 395,
+                         'min': 80,
+                         'outliers': [795]},
+                   'b': {'25%': 485.5,
+                         '50%': 673.0,
+                         '75%': 745.0,
+                         'iqr': 259.5,
+                         'max': 867,
+                         'min': 158,
+                         'outliers': []},
+                   'c': {'25%': 336.0,
+                         '50%': 512.5,
+                         '75%': 629.5,
+                         'iqr': 293.5,
+                         'max': 652,
+                         'min': 135,
+                         'outliers': []},
+                   'd': {'25%': 668.0,
+                         '50%': 682.0,
+                         '75%': 738.0,
+                         'iqr': 70.0,
+                         'max': 794,
+                         'min': 654,
+                         'outliers': []}}
+    res = plot(df_data, 'y', 'x')
+
+    assert res['a'] == df_expected['a']
+    assert res['b'] == df_expected['b']
+    assert res['c'] == df_expected['c']
+    assert res['d'] == df_expected['d']
+
+    df_expected_2 = {('a', Timestamp('1898-01-02 00:00:00')): 1,
+                     ('a', Timestamp('1950-12-09 00:00:00')): 3,
+                     ('a', Timestamp('1990-12-09 00:00:00')): 1,
+                     ('b', Timestamp('1898-01-02 00:00:00')): 1,
+                     ('b', Timestamp('1950-12-09 00:00:00')): 7,
+                     ('c', Timestamp('1898-01-02 00:00:00')): 1,
+                     ('c', Timestamp('1950-12-09 00:00:00')): 2,
+                     ('d', Timestamp('1950-12-09 00:00:00')): 1,
+                     ('d', Timestamp('1990-12-09 00:00:00')): 1,
+                     ('d', Timestamp('2011-07-04 00:00:00')): 1
+                    }
+
+    res_2 = plot(df_data, 'x', 'somedate')
+    assert df_expected_2 == res_2
+
+def test_corner():
+    """
+
+    :return:
+    """
+    df_2 = pd.DataFrame(
+        {'all_nan': [np.nan for _ in range(10)], 'all_one': np.ones(10),
+         'all_zeros': np.zeros(10), 'random': np.array(
+             [0.38538395, 0.13609054, 0.15973238, 0.96192966, 0.03708882,
+              0.03633855, 0.25260128, 0.72139843, 0.74553949,
+              0.41102021])})
+
+    df_1_expected = {'all_one': {1.0: 10},
+                     'all_zeros': {0.0: 10},
+                     'random': np.array([2, 2, 1, 1, 1, 0, 0, 2, 0, 1],
+                                        dtype=np.int64)}
+
+    res = plot(df_2, force_cat=['all_one', 'all_zeros'])
+
+    # assert np.all(res['all_nan'] ==  .df_1_expected['all_nan'])
+    assert res['all_one'] == df_1_expected['all_one']
+    assert res['all_zeros'] == df_1_expected['all_zeros']
+
+    df_2 = pd.DataFrame({
+        'empty': [],
+        'another_empty': []
+    })
+
+    df_2_expected = dict()
+
+    res = plot(df_2, 'empty', 'another_empty')
+    assert res == df_2_expected
