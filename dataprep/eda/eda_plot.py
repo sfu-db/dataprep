7813c364c6ff8d998901fc9c4543dff59e0e9bc9
Waterpine <songbian@zju.edu.cn>
2019-06-16 23:32:51 +0800

    fix datatype

--- dataprep/eda/eda_plot.py
+++ dataprep/eda/eda_plot.py
@@ -331,7 +331,7 @@ def _calc_div(matrix: np.ndarray,
     return matrix / arr[:, None] / arr[None, :]
 
 
-def _calc_kendall(a: np.ndarray, b: np.ndarray) -> float:
+def _calc_kendall(a: np.ndarray, b: np.ndarray) -> Any:
     kendallta, _ = kendalltau(a, b)
     return kendallta
 
