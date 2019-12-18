55aaee5837920631a98d3d06369ff2b377ae907a
Weiyuan Wu <doomsplayer@gmail.com>
2019-12-18 12:25:06 -0800

    fix(eda): fix not recognizing categorical dtype

--- dataprep/eda/dtypes.py
+++ dataprep/eda/dtypes.py
@@ -8,8 +8,13 @@ from typing import Any
 import numpy as np
 import pandas as pd
 
-CATEGORICAL_DTYPES = [pd.CategoricalDtype, np.bool, np.object, pd.PeriodDtype]
-NUMERICAL_DTYPES = [np.number, np.datetime64, pd.DatetimeTZDtype]
+CATEGORICAL_NUMPY_DTYPES = [np.bool, np.object]
+CATEGORICAL_PANDAS_DTYPES = [pd.CategoricalDtype, pd.PeriodDtype]
+CATEGORICAL_DTYPES = CATEGORICAL_NUMPY_DTYPES + CATEGORICAL_PANDAS_DTYPES
+
+NUMERICAL_NUMPY_DTYPES = [np.number, np.datetime64]
+NUMERICAL_PANDAS_DTYPES = [pd.DatetimeTZDtype]
+NUMERICAL_DTYPES = NUMERICAL_NUMPY_DTYPES + NUMERICAL_PANDAS_DTYPES
 
 
 class DType(Enum):
@@ -25,12 +30,16 @@ def is_categorical(dtype: Any) -> bool:
     """
     Given a type, return if that type is a categorical type
     """
+
+    if is_numerical(dtype):
+        return False
+
     if isinstance(dtype, np.dtype):
         dtype = dtype.type
 
-    return not is_numerical(dtype) and any(
-        issubclass(dtype, c) for c in CATEGORICAL_DTYPES
-    )
+        return any(issubclass(dtype, c) for c in CATEGORICAL_NUMPY_DTYPES)
+    else:
+        return any(isinstance(dtype, c) for c in CATEGORICAL_PANDAS_DTYPES)
 
 
 def is_numerical(dtype: Any) -> bool:
@@ -39,5 +48,6 @@ def is_numerical(dtype: Any) -> bool:
     """
     if isinstance(dtype, np.dtype):
         dtype = dtype.type
-
-    return any(issubclass(dtype, c) for c in NUMERICAL_DTYPES)
+        return any(issubclass(dtype, c) for c in NUMERICAL_NUMPY_DTYPES)
+    else:
+        return any(isinstance(dtype, c) for c in NUMERICAL_PANDAS_DTYPES)
