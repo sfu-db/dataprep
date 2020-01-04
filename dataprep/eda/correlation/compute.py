fix(eda.correlation): plot_correlation only supports for numerical data

dataprep/eda/correlation/compute.py
@@ -15,7 +15,7 @@ import pandas as pd
 from scipy.stats import kendalltau
 
 from ...errors import UnreachableError
-from ..dtypes import NUMERICAL_DTYPES, is_categorical, is_numerical
+from ..dtypes import NUMERICAL_DTYPES
 from ..intermediate import Intermediate
 from ..utils import to_dask
 
@@ -77,7 +77,8 @@ def compute_correlation(
         ), "value_range and k cannot be present in both"
 
         df = df.select_dtypes(NUMERICAL_DTYPES)
-        assert len(df.columns) != 0, f"No numerical columns found"
+        if len(df.columns) == 0:
+            return Intermediate(visual_type=None)
 
         data = df.to_dask_array()
         # TODO Can we remove this? Without the computing, data has unknown rows so da.cov will fail.
@@ -115,7 +116,8 @@ def compute_correlation(
         )
     elif x is not None and y is None:
         df = df.select_dtypes(NUMERICAL_DTYPES)
-        assert len(df.columns) != 0, f"No numerical columns found"
+        if len(df.columns) == 0:
+            return Intermediate(visual_type=None)
         assert x in df.columns, f"{x} not in numerical column names"
 
         columns = df.columns[df.columns != x]
@@ -148,15 +150,8 @@ def compute_correlation(
         assert value_range is None
         assert x in df.columns, f"{x} not in columns names"
         assert y in df.columns, f"{y} not in columns names"
-
-        xdtype = df[x].dtype
-        ydtype = df[y].dtype
-        # pylint: disable=no-else-raise
-        if is_categorical(xdtype) and is_categorical(ydtype):
-            raise NotImplementedError
-            # intermediate = _cross_table(df=df, x=x, y=y)
-            # return intermediate
-        elif is_numerical(xdtype) and is_numerical(ydtype):
+        df = df.select_dtypes(NUMERICAL_DTYPES)
+        if x in df.columns and y in df.columns:
             coeffs, df, influences = scatter_with_regression(
                 df[x].values.compute_chunk_sizes(),
                 df[y].values.compute_chunk_sizes(),
@@ -181,9 +176,7 @@ def compute_correlation(
 
             return Intermediate(**result, visual_type="correlation_scatter")
         else:
-            raise ValueError(
-                "Cannot calculate the correlation between two different dtype column"
-            )
+            return Intermediate(visual_type=None)
 
     raise UnreachableError
 