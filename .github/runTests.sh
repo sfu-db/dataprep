4916a81ffa3272b6c6e42df6296dc5bf13522522
Yejia Liu <yejia@d207-023-181-164.wireless.sfu.ca>
2020-03-09 12:07:39 -0700

    fix test bug

--- .github/runTests.sh
+++ .github/runTests.sh
@@ -8,7 +8,7 @@ do
                 parentdir="$(basename "$(dirname "$file")")"
                 echo ${parentdir}
                 name="${filename%.*}"
-                test_file_name=dataprep/tests/${parentdir}/${name}_test.py
+                test_file_name=tests/${parentdir}/${name}_test.py
                 echo ${test_file_name}
                 python test_file_name
         fi
