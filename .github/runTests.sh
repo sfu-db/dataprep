85a6a7d317ce74dd4c58c175c5d8f5f1dc7467bb
Yejia Liu <yejia@Yejias-MacBook-Pro.local>
2020-03-08 23:23:24 -0700

    python tests

--- .github/runTests.sh
+++ .github/runTests.sh
@@ -7,7 +7,8 @@ do
                 parentdir="$(basename "$(dirname "$file")")"
                 echo ${parentdir}
                 name="${filename%.*}"
-                test_file_name=${parentdir}/${name}_test.py
+                test_file_name=dataprep/tests/${parentdir}/${name}_test.py
                 echo ${test_file_name}
+                python test_file_name
         fi
 done
