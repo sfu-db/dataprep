0cfe89375feed16211711ac4401c75a5b29929e5
Yejia Liu <yejia@Yejias-MacBook-Pro.local>
2020-03-11 02:10:30 -0700

    build poetry env

--- .github/runTests.sh
+++ .github/runTests.sh
@@ -11,11 +11,11 @@ do
                 name="${filename%.*}"
                 test_file_name=./dataprep/tests/${parentdir}/${name}_test.py
                 echo ${test_file_name}
-                poetry run python test_file_name
+                poetry run pytest test_file_name
         fi
 done
 poetry run pytest ./dataprep/tests/eda/test.py
-# poetry run python ./dataprep/tests/eda/test_plot.py
-# poetry run python ./dataprep/tests/eda/test_plot_correlation.py
-# poetry run python ./dataprep/tests/eda/test_plot_missing.py
+poetry run python ./dataprep/tests/eda/test_plot.py
+poetry run python ./dataprep/tests/eda/test_plot_correlation.py
+poetry run python ./dataprep/tests/eda/test_plot_missing.py
 
