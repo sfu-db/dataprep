4a4a6447656a57f76c049b2fe0ddc7450b067fd7
Weiyuan Wu <doomsplayer@gmail.com>
2020-03-18 23:55:50 -0700

    build: manage release by semantic-release

--- dataprep/__init__.py
+++ dataprep/__init__.py
@@ -2,7 +2,23 @@
     Data preparation module
 """
 import logging
+from pathlib import Path
+from typing import cast
+
+import toml
 
 DEFAULT_PARTITIONS = 1
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def get_version() -> str:
+    """
+    Get the library version from pyproject.toml
+    """
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    return cast(str, pyproject["tool"]["poetry"]["version"])
+
+
+__version__ = get_version()
