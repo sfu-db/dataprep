b7b83acfe706ed6b331062a2d79521d0288f0bdd
Weiyuan Wu <doomsplayer@gmail.com>
2020-03-20 15:02:21 -0700

    fix: no pyproject in the installed library

--- dataprep/__init__.py
+++ dataprep/__init__.py
@@ -11,14 +11,4 @@ DEFAULT_PARTITIONS = 1
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-
-def get_version() -> str:
-    """
-    Get the library version from pyproject.toml
-    """
-    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
-    pyproject = toml.loads(open(str(path)).read())
-    return cast(str, pyproject["tool"]["poetry"]["version"])
-
-
-__version__ = get_version()
+__version__ = "0.2.0"
