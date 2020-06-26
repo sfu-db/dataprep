b72770a6a43f3d43aaa7f525348ec99b8de28e5f
Weiyuan Wu <doomsplayer@gmail.com>
2020-06-25 17:31:34 -0700

    fix(eda): fix the plot doesn't show up

--- dataprep/eda/report.py
+++ dataprep/eda/report.py
@@ -9,6 +9,26 @@ from bokeh.io import save
 from bokeh.models import LayoutDOM
 from bokeh.resources import CDN
 from IPython.display import HTML, display
+from jinja2 import Template
+
+INLINE_TEMPLATE = Template(
+    """
+{% from macros import embed %}
+{% block inner_body %}
+    {% block contents %}
+    {% for doc in docs %}
+        {{ embed(doc) if doc.elementid }}
+        {% for root in doc.roots %}
+        {% block root scoped %}
+            {{ embed(root) | indent(10) }}
+        {% endblock %}
+        {% endfor %}
+    {% endfor %}
+    {% endblock %}
+    {{ plot_script | indent(8) }}
+{% endblock %}
+"""
+)
 
 
 class Report:
@@ -25,7 +45,12 @@ class Report:
         """
         save function
         """
-        save(self.to_render, filename=filename, resources=CDN, title="Report")
+        save(
+            self.to_render,
+            filename=filename,
+            resources=CDN,
+            title="DataPrep.EDA Report",
+        )
 
     def _repr_html_(self) -> str:
         # Windows forbids us open the file twice as the result bokeh cannot
@@ -33,15 +58,28 @@ class Report:
         with NamedTemporaryFile(suffix=".html", delete=False) as tmpf:
             pass
 
-        save(self.to_render, filename=tmpf.name, resources=CDN, title="Report")
+        save(
+            self.to_render,
+            filename=tmpf.name,
+            resources=CDN,
+            template=INLINE_TEMPLATE,
+            title="DataPrep.EDA Report",
+        )
         with open(tmpf.name, "r") as f:
             output_html = f.read()
 
         # Delete the temporary file
         Path(tmpf.name).unlink()
 
+        # Fix the bokeh: bokeh wrongly call the "waiting for bokeh to load" function
+        # inside "Bokeh.safely", which causes Bokeh not found because
+        # Bokeh is even not loaded!
+        patched_html = output_html.replace(
+            "Bokeh.safely",
+            "var __dataprep_bokeh_fix = (f) => document.Bokeh === undefined ? setTimeout(f, 1000) : f(); __dataprep_bokeh_fix",  # pylint: disable=line-too-long
+        )
         # embed into report template created by us here
-        return output_html
+        return patched_html
 
     def show(self) -> None:
         """
