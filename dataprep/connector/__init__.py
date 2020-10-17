a96d9b3c213ba47cd9f9c905bc6a333505876b75
Weiyuan Wu <youngw@sfu.ca>
2020-10-17 14:25:33 -0700

    feat(connector): simplify generator, add connect

--- dataprep/connector/__init__.py
+++ dataprep/connector/__init__.py
@@ -1,6 +1,57 @@
-"""
-DataConnector
-"""
-from .connector import Connector
+"""Connector"""
 
-__all__ = ["Connector"]
+from typing import Any, Dict, Optional
+
+from .connector import Connector
+from .generator import ConfigGenerator, ConfigGeneratorUI
+
+__all__ = ["Connector", "ConfigGenerator", "ConfigGeneratorUI", "connect"]
+
+
+def connect(
+    config_path: str,
+    *,
+    update: bool = False,
+    _auth: Optional[Dict[str, Any]] = None,
+    _concurrency: int = 1,
+    **kwargs: Any,
+) -> Connector:
+    """Connect to a website.
+
+    Parameters
+    ----------
+    config_path
+        The path to the config. It can be hosted, e.g. "yelp", or from
+        local filesystem, e.g. "./yelp"
+    _auth: Optional[Dict[str, Any]] = None
+        The parameters for authentication, e.g. OAuth2
+    _concurrency: int = 5
+        The concurrency setting. By default it is 1 reqs/sec.
+    update: bool = True
+        Force update the config file even if the local version exists.
+    **kwargs
+        Parameters that shared by different queries.
+
+    Returns
+    -------
+    Connector
+        a Connector object.
+
+    Example
+    -------
+    >>> from dataprep.connector import connect
+    >>> dc = connect("yelp", _auth={"access_token": access_token}, _concurrency=3)
+    """
+    return Connector(config_path, update=update, _auth=_auth, _concurrency=_concurrency, **kwargs)
+
+
+def config_generator_ui(existing: Optional[Dict[str, Any]] = None) -> None:
+    """Create a Config Generator UI.
+
+    Parameters
+    ----------
+    existing: Optional[Dict[str, Any]] = None
+        Optionally pass in an existing configuration.
+    """
+
+    ConfigGeneratorUI(existing).display()
