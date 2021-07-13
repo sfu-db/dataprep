0a634d2b6b3a0b88962ea8cc6e9e8dd18d7c0b6a
github-action-benchmark <github@users.noreply.github.com>
2021-07-13 23:25:48 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for d6a6af6ee2815fa03e117fe0085712d6734c5e4b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626213625929,
+  "lastUpdate": 1626218748324,
   "repoUrl": "https://github.com/sfu-db/dataprep",
   "entries": {
     "DataPrep.EDA Benchmarks": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.01519471045276923",
             "extra": "mean: 5.027136754200001 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jlpengcs@gmail.com",
+            "name": "Jinglin Peng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6a6af6ee2815fa03e117fe0085712d6734c5e4b",
+          "message": "Merge pull request #671 from sfu-db/readme_benchmark\n\ndocs(readme): add benchmark link",
+          "timestamp": "2021-07-13T16:22:30-07:00",
+          "tree_id": "14bd764dd31470a0eafb1f1f5a7fa7e6c66a2ce5",
+          "url": "https://github.com/sfu-db/dataprep/commit/d6a6af6ee2815fa03e117fe0085712d6734c5e4b"
+        },
+        "date": 1626218746886,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.1666758029896957,
+            "unit": "iter/sec",
+            "range": "stddev: 0.23061170237253145",
+            "extra": "mean: 5.999671110400004 sec\nrounds: 5"
           }
         ]
       }
