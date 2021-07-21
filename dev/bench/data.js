acbe77699c481d01270e293c9c3165da85af1b1f
github-action-benchmark <github@users.noreply.github.com>
2021-07-21 00:35:07 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for 19077c6810bf1b684792dfbc2ebb2c690928bda9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626218748324,
+  "lastUpdate": 1626827705859,
   "repoUrl": "https://github.com/sfu-db/dataprep",
   "entries": {
     "DataPrep.EDA Benchmarks": [
@@ -62,6 +62,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.23061170237253145",
             "extra": "mean: 5.999671110400004 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jlpengcs@gmail.com",
+            "name": "Jinglin Peng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "19077c6810bf1b684792dfbc2ebb2c690928bda9",
+          "message": "Merge pull request #674 from Waterpine/song\n\ntest(eda): add test for config",
+          "timestamp": "2021-07-20T17:32:09-07:00",
+          "tree_id": "b1eb050b9d5305e56f83e37355d96cdc45c0b860",
+          "url": "https://github.com/sfu-db/dataprep/commit/19077c6810bf1b684792dfbc2ebb2c690928bda9"
+        },
+        "date": 1626827704362,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.180197762286889,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10882099934591108",
+            "extra": "mean: 5.549458479999998 sec\nrounds: 5"
           }
         ]
       }
