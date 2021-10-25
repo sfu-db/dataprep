1e0dfecadf9d9a9a71ff1b1f27754e99125f48e3
Weiyuan Wu <youngw@sfu.ca>
2021-10-25 22:52:42 +0000

    remove yelp test

--- dataprep/tests/connector/test_integration.py
+++ dataprep/tests/connector/test_integration.py
@@ -8,31 +8,31 @@ from ...utils import display_dataframe
 from ...connector.utils import Request
 
 
-@pytest.mark.skipif(
-    environ.get("DATAPREP_CREDENTIAL_TESTS", "0") == "0",
-    reason="Skip tests that requires credential",
-)
-def test_connector() -> None:
-    token = environ["DATAPREP_DATA_CONNECTOR_YELP_TOKEN"]
-    dc = Connector("yelp", _auth={"access_token": token}, _concurrency=3)
-    df = asyncio.run(dc.query("businesses", term="ramen", location="vancouver"))
-
-    assert len(df) > 0
-
-    websites()
-
-    dc.info()
-
-    display_dataframe(df)
-
-    df = asyncio.run(dc.query("businesses", _count=120, term="ramen", location="vancouver"))
-
-    assert len(df) == 120
-
-    df = asyncio.run(dc.query("businesses", _count=1000, term="ramen", location="vancouver"))
-
-    assert len(df) < 1000
-
+# @pytest.mark.skipif(
+#     environ.get("DATAPREP_CREDENTIAL_TESTS", "0") == "0",
+#     reason="Skip tests that requires credential",
+# )
+# def test_connector() -> None:
+#    token = environ["DATAPREP_DATA_CONNECTOR_YELP_TOKEN"]
+#    dc = Connector("yelp", _auth={"access_token": token}, _concurrency=3)
+#    df = asyncio.run(dc.query("businesses", term="ramen", location="vancouver"))
+#
+#    assert len(df) > 0
+#
+#    websites()
+#
+#    dc.info()
+#
+#    display_dataframe(df)
+#
+#    df = asyncio.run(dc.query("businesses", _count=120, term="ramen", location="vancouver"))
+#
+#    assert len(df) == 120
+#
+#    df = asyncio.run(dc.query("businesses", _count=1000, term="ramen", location="vancouver"))
+#
+#    assert len(df) < 1000
+#
 
 @pytest.mark.skipif(
     environ.get("DATAPREP_CREDENTIAL_TESTS", "0") == "0",
