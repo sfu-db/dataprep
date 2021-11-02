7f4ab12a26960a5a7df7882b971880f13ace38dc
qidanrui <qidanrui@gmail.com>
2021-11-02 14:15:46 -0700

    feat(clean): Add wiki

---
+++ dataprep/clean/clean_df_gui.py
@@ -0,0 +1,51 @@
+"""
+Conduct a set of operations that would be useful for
+cleaning and standardizing a full Pandas DataFrame.
+"""
+# pylint: disable-msg=relative-beyond-top-level
+# pylint: disable-msg=cyclic-import
+# type: ignore
+
+from typing import Any
+
+import pandas as pd
+
+from IPython.display import IFrame, display
+
+from dataprep.clean.gui.clean_gui import launch
+
+
+def clean_df_gui(
+    df: pd.DataFrame,
+) -> Any:
+    """
+    This function shows the GUI of clean module.
+
+    Parameters
+    ----------
+    df
+        A Pandas DataFrame containing the data to be cleaned.
+    """
+    # pylint: disable=too-many-arguments
+    # pylint: disable-msg=too-many-locals
+    # pylint:disable=too-many-branches
+    # type: ignore
+
+    return UserInterface(df).display()
+
+
+class UserInterface:
+    """
+    A user interface used by clean module.
+    """
+
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def display(self) -> None:
+        """Display the GUI."""
+        launch(self.df)
+
+        path_to_local_server = "http://localhost:7680"
+        display(IFrame(path_to_local_server, width=900, height=500))
---
+++ dataprep/clean/gui/clean_frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
