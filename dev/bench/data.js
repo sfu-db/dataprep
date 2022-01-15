1e07a7cb7cd605a8175a438004e483dfe2734b16
github-action-benchmark <github@users.noreply.github.com>
2022-01-15 20:30:38 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for bf336dec5c6587b5fbeac7f0b647ed8e5ff40d0a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640412961782,
+  "lastUpdate": 1642278637763,
   "repoUrl": "https://github.com/sfu-db/dataprep",
   "entries": {
     "DataPrep.EDA Benchmarks": [
@@ -2015,6 +2015,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.058753052256889135",
             "extra": "mean: 3.3712653122000007 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "youngw@sfu.ca",
+            "name": "Weiyuan Wu",
+            "username": "dovahcrow"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf336dec5c6587b5fbeac7f0b647ed8e5ff40d0a",
+          "message": "fix 404 link for connector\n\nfixes #783",
+          "timestamp": "2022-01-15T12:27:01-08:00",
+          "tree_id": "15134d007fefeaf8a1d31479a3e371434611289a",
+          "url": "https://github.com/sfu-db/dataprep/commit/bf336dec5c6587b5fbeac7f0b647ed8e5ff40d0a"
+        },
+        "date": 1642278632760,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.2461884801977018,
+            "unit": "iter/sec",
+            "range": "stddev: 0.08392425551515681",
+            "extra": "mean: 4.061928483399993 sec\nrounds: 5"
           }
         ]
       }
