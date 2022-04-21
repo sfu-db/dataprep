a7bf8206bec80d0384e1baa25408588d5d9e1c90
Jinglin Peng <jlpengcs@gmail.com>
2022-04-21 16:12:42 -0700

    feat(eda): add get_db_names

--- dataprep/tests/datasets/test_datasets.py
+++ dataprep/tests/datasets/test_datasets.py
@@ -1,11 +1,16 @@
 """
 module for testing the functions inside datasets
 """
-from ...datasets import get_dataset_names, load_dataset
+from ...datasets import get_dataset_names, get_db_names, load_dataset, load_db
 
 
 def test_get_dataset_names() -> None:
     names = get_dataset_names()
+    assert len(names) > 0
+
+
+def test_get_db_names() -> None:
+    names = get_db_names()
     assert len(names) > 0
 
 
@@ -14,3 +19,9 @@ def test_load_dataset() -> None:
     for name in dataset_names:
         df = load_dataset(name)
         assert len(df) > 0
+
+
+def test_load_db() -> None:
+    dataset_names = get_db_names()
+    for name in dataset_names:
+        db = load_db(name)
